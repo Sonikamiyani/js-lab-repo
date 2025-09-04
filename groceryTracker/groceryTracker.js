599f95bdc20e5f79a8c605f879e963e52fa93176
Sonika Miyani <miyanisonika67@gmail.com>
2025-09-04 15:31:53 -0400

    message

---
+++ groceryTracker/groceryTracker.js
@@ -0,0 +1,13 @@
+let amount1 ,amount2 ,amount3;
+
+
+function groceryTracker() {
+ amount1 = parseFloat(document.getElementById('amount1').value);
+ amount2 = parseFloat(document.getElementById('amount2').value);
+ amount3 = parseFloat(document.getElementById('amount3').value);
+ 
+let groceryTracker = amount1 + amount2 + amount3;
+ document.getElementById('result').innerText = `The final amount of grocery is: ${groceryTracker}`;
+ 
+}
+
